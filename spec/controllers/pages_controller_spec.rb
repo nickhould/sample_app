4641828337b59270b62fc8cfa37be38ebfdd67c9
Jean-Nicholas <nick.hould@gmail.com>
2011-09-11 12:59:04 -0400

    Added stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -10,7 +15,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | Home")
     end
     
     it "should have a non-blank body" do
@@ -26,7 +32,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | Contact")
     end
   end
   
@@ -37,7 +44,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | About")
     end
   end
 end
