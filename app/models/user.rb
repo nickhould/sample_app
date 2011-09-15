bb2cb30ccd82c65346be28586a1316eba15c47e8
Jean-Nicholas <nick.hould@gmail.com>
2011-09-14 20:20:34 -0400

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
-class User < ActiveRecord::Base
-  
-  attr_accessible :name, email
-  
-end
-
 # == Schema Information
 #
 # Table name: users
@@ -14,4 +8,20 @@ end
 #  created_at :datetime
 #  updated_at :datetime
 #
+
+class User < ActiveRecord::Base
+  
+  email_regex = /\A[\w+.\-]+@[a-z.\d\-]+\.[a-z]+\z/i
+  
+  attr_accessible :name, :email
+  
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50}
+  validates :email, :presence => true,
+                    :format => { :with => email_regex},
+                    :uniqueness => {:case_sensitive => false}
+  
+end
+
+
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110914002418) do
+ActiveRecord::Schema.define(:version => 20110915001738) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,5 +18,7 @@ ActiveRecord::Schema.define(:version => 20110914002418) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
